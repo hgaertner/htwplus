8323cd08a7cad6740efeeabca235d4008c5b8b04
Fabi Kirstein <linsenfips@gmail.com>
2013-05-04 15:07:52 +0200

    Added BaseModel

--- app/models/Course.java
+++ app/models/Course.java
@@ -4,17 +4,16 @@ import java.util.*;
 import play.data.validation.Constraints.*;
 import play.data.format.Formats.*;
 import javax.persistence.*;
+
+import models.base.BaseModel;
+
 import org.hibernate.validator.constraints.Length;
 import play.db.jpa.*;
 
 @Entity
-@SequenceGenerator(name = "course_seq", sequenceName = "course_seq")
-public class Course {
+@SequenceGenerator(name = "default_seq", sequenceName = "course_seq")
+public class Course extends BaseModel {
 
-	@Id
-	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "course_seq")
-	public Long id;
-	
 	@Required
 	public String title;
 	
@@ -26,24 +25,10 @@ public class Course {
 	//public String ownerId;
 	//public String member;
 
-	public Date createdAt;
-	
-	public Date updatedAt;
-	
 	@Required
 	@Length(min = 4, max = 45)
 	public String token;
-	
-	
-	@PrePersist
-	void createdAt() {
-		this.createdAt = this.updatedAt = new Date();
-	}
 
-	@PreUpdate
-	void updatedAt() {
-		this.updatedAt = new Date();
-	}
 	
 	public static List<Course> all() {
 		List<Course> courses = JPA.em().createQuery("SELECT t FROM Course t").getResultList();
@@ -54,15 +39,18 @@ public class Course {
 		return JPA.em().find(Course.class, id);
 	}
 	
+	@Override
 	public void create() {
 		JPA.em().persist(this);
 	}
 	
+	@Override
 	public void update(Long id) {
 		this.id = id;
 		JPA.em().merge(this);
 	}
 	
+	@Override
 	public void delete() {
 		JPA.em().remove(this);
 	}
---
+++ app/models/base/BaseModel.java
@@ -0,0 +1,43 @@
+package models.base;
+
+import java.util.*;
+import play.data.validation.Constraints.*;
+import play.data.format.Formats.*;
+import javax.persistence.*;
+
+import models.Course;
+
+import org.hibernate.annotations.Columns;
+import org.hibernate.validator.constraints.Length;
+import play.db.jpa.*;
+
+@MappedSuperclass
+public abstract class BaseModel {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "default_seq")
+	public Long id;
+	
+	@Column(name = "created_at")
+	public Date createdAt;
+	@Column(name = "updated_at")
+	public Date updatedAt;
+		
+	@PrePersist
+	void createdAt() {
+		this.createdAt = this.updatedAt = new Date();
+	}
+
+	@PreUpdate
+	void updatedAt() {
+		this.updatedAt = new Date();
+	}
+
+	public abstract void create();
+		
+	public abstract void update(Long id);
+	
+	public abstract void delete();
+	
+	
+}
