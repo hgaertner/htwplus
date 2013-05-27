7508a4bb134ff74b3661497e269622c1db65b0da
Bjoern Ahlfeld <b.ahlfeld@gmail.com>
2013-05-27 20:20:49 +0200

    Post can be created in courses and groups

--- app/controllers/CourseController.java
+++ app/controllers/CourseController.java
@@ -1,5 +1,8 @@
-
 package controllers;
+
+import java.util.List;
+
+import org.hibernate.metamodel.source.binder.JpaCallbackClass;
 
 import play.*;
 import play.mvc.*;
@@ -13,19 +16,47 @@ import play.mvc.Security;
 @Security.Authenticated(Secured.class)
 @Transactional
 public class CourseController extends Controller {
-  
+
 	static Form<Course> courseForm = Form.form(Course.class);
 	
+	static Form<Post> postForm = Form.form(Post.class);
+
 	public static Result index() {
 		return ok(index.render(Course.all()));
 	}
-    
-    public static Result add() {
-    	return ok(add.render(courseForm));
-    }
-    
-    public static Result create() {
-    	Form<Course> filledForm = courseForm.bindFromRequest();
+
+	public static Result add() {
+		return ok(add.render(courseForm));
+	}
+
+	public static Result addPost(long courseId) {
+		// try{
+		Form<Post> filledForm = postForm.bindFromRequest();
+		if (filledForm.hasErrors()) {
+			flash("message", "Error in Form!");
+			// return badRequest(view.render(arg0, arg1));
+		} else {
+			Post p = filledForm.get();
+			try{
+				final long accountId = Long.parseLong(session().get("id"));
+				Post p2 = new Post();
+				p2.content = p.content;
+				//if(currentCourse.members.contains(currentUser)){
+					p2.owner = JPA.em().find(Account.class, accountId);
+					p2.course = JPA.em().find(Course.class, courseId);
+					p2.create();
+				//} else {
+					//TODO return error Message current user is not a member of the course...
+				//}
+			}catch(NumberFormatException exp){
+				//TODO log exception and handle it...
+			}
+		}
+		return view(courseId);
+	}
+
+	public static Result create() {
+		Form<Course> filledForm = courseForm.bindFromRequest();
 		if (filledForm.hasErrors()) {
 			flash("message", "Error in Form!");
 			return badRequest(add.render(filledForm));
@@ -35,45 +66,50 @@ public class CourseController extends Controller {
 			flash("message", "Created new Course!");
 			return redirect(routes.CourseController.index());
 		}
-    }
-    
-    public static Result view(Long id) {
-    	Course course = Course.findById(id);
-    	if(course == null) {
-    		return redirect(routes.CourseController.index());
-    	} else {
-        	return ok(view.render(course));
-    	}
-    }
-    
-    public static Result edit(Long id) {
-    	Course course = Course.findById(id);
-    	if(course == null) {
-    		return redirect(routes.CourseController.index());
-    	} else {
-        	return ok(edit.render(course.id, courseForm.fill(course)));
-    	}
-    }
-    
-    public static Result update(Long id) {
-    	Course course = Course.findById(id);
-    	Form<Course> filledForm = courseForm.bindFromRequest();
+	}
+
+	@Transactional
+	public static Result view(Long id) {
+		Course course = Course.findById(id);
+		if (course == null) {
+			return redirect(routes.CourseController.index());
+		} else {
+			
+			  List<Post> posts = JPA.em()
+			  .createQuery("SELECT p FROM Post p WHERE p.course.id = ?1")
+			 .setParameter(1, id).getResultList();
+			 
+			return ok(view.render(course, postForm, posts));
+		}
+	}
+
+	public static Result edit(Long id) {
+		Course course = Course.findById(id);
+		if (course == null) {
+			return redirect(routes.CourseController.index());
+		} else {
+			return ok(edit.render(course.id, courseForm.fill(course)));
+		}
+	}
+
+	public static Result update(Long id) {
+		Course course = Course.findById(id);
+		Form<Course> filledForm = courseForm.bindFromRequest();
 		if (filledForm.hasErrors()) {
 			flash("message", "Error in Form!");
-			return badRequest(edit.render(id ,filledForm));
+			return badRequest(edit.render(id, filledForm));
 		} else {
 			filledForm.get().update(id);
 			flash("message", "Updated Course!");
 			return redirect(routes.CourseController.index());
-		}		
-    }
-    
-    public static Result delete(Long id) {
-    	Course course = Course.findById(id);
-    	course.delete();
-    	flash("message", "Course " + course.title + " deleted!");
-    	return redirect(routes.CourseController.index());
-    }
-    
-  
+		}
+	}
+
+	public static Result delete(Long id) {
+		Course course = Course.findById(id);
+		course.delete();
+		flash("message", "Course " + course.title + " deleted!");
+		return redirect(routes.CourseController.index());
+	}
+
 }
--- app/controllers/GroupController.java
+++ app/controllers/GroupController.java
@@ -1,10 +1,14 @@
 package controllers;
 
+import java.util.List;
+
 import play.*;
 import play.mvc.*;
 import play.data.*;
 import models.Account;
+import models.Course;
 import models.Group;
+import models.Post;
 import views.html.Group.*;
 import play.db.jpa.*;
 
@@ -13,18 +17,59 @@ import play.db.jpa.*;
 public class GroupController extends Controller {
 
 	static Form<Group> groupForm = Form.form(Group.class);
+	
+	static Form<Post> postForm = Form.form(Post.class);
 
 	public static Result index() {
 		return ok(index.render(Group.all()));
 	}
+	
+	public static Result addPost(long groupId) {
+		// try{
+		Form<Post> filledForm = postForm.bindFromRequest();
+		if (filledForm.hasErrors()) {
+			flash("message", "Error in Form!");
+			// return badRequest(view.render(arg0, arg1));
+		} else {
+			Post p = filledForm.get();
+			try{
+				final long accountId = Long.parseLong(session().get("id"));
+				Post p2 = new Post();
+				p2.content = p.content;
+				//if(currentCourse.members.contains(currentUser)){
+					p2.owner = JPA.em().find(Account.class, accountId);
+					p2.group = JPA.em().find(Group.class, groupId);
+					p2.create();
+				//} else {
+					//TODO return error Message current user is not a member of the course...
+				//}
+			}catch(NumberFormatException exp){
+				//TODO log exception and handle it...
+			}
+		}
+		return view(groupId);
+	}
+	
 
 	public static Result view(Long id) {
 		Group group = Group.findById(id);
 		if (group == null) {
 			return redirect(routes.GroupController.index());
 		} else {
-			return ok(view.render(group));
+			Form<Post> formPost = Form.form(Post.class);
+			
+			List<Post> posts = JPA.em()
+			  .createQuery("SELECT p FROM Post p WHERE p.group.id = ?1")
+			 .setParameter(1, id).getResultList();
+			return ok(view.render(group,posts,postForm));
 		}
+	}
+	
+	public static Result deletePost(Long id){
+		Post p = Post.findById(id);
+		p.delete(Long.parseLong(session().get("id")));
+		
+		return view(id);
 	}
 
 	public static Result create() {
