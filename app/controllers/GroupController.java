6ea1158b8e651057400221c27e4b095a11a8be98
Bjoern Ahlfeld <b.ahlfeld@gmail.com>
2013-12-07 11:54:32 +0100

    Redirect to group after deleting a comment

--- app/controllers/GroupController.java
+++ app/controllers/GroupController.java
@@ -201,12 +201,13 @@ public class GroupController extends BaseController {
 		for (Map.Entry<String, String[]> entry : entries) {
 			if (entry.getKey().equals("keyword")) {
 				final String keyword = entry.getValue()[0];
-				Logger.debug("Value of key" + keyword);
+				
 				Navigation.set(Level.GROUPS, "Suchergebnisse");
 				courseResults = Group.searchForCourseByKeyword(keyword);
 				groupResults = Group.searchForGroupByKeyword(keyword);
 				
 				accResults = Account.searchForAccountByKeyword(keyword);
+				Logger.info("Sizes: " + "Groups: " +groupResults.size() + " Courses: " + courseResults.size() + " Account: " +accResults.size());
 			}
 
 		}
